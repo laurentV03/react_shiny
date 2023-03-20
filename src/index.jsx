721841c871f9b22fdfb685c7720b6190f2dca15a
laurentV03 <mapstom1@gmail.com>
2023-03-20 17:26:22 +0100

    réinstallation : router v5 avec react v17 (reprise du package.json du cours) React Router PropTypes

---
+++ src/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './index.css'
+
+// import App from './App';
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Freelances from './pages/Freelances'
+import Results from './pages/Results'
+
+import Header from './components/Header'
+import Error from './components/Error'
+
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals()
